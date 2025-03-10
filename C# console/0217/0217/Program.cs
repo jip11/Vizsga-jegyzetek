cf62af2b7ce8f71b108130fccf9e1710c668af98
jip11 <58308465+jip11@users.noreply.github.com>
2025-03-10 13:50:53 +0100

    c#

---
+++ C# console/0217/0217/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Lajhar
+{
+    public required string Nev { get; set; }
+    public int Eletkor { get; set; }
+    public double Testtomeg { get; set; }
+    public required string Nem { get; set; }
+    public int Utodok { get; set; }
+}
+namespace _0217
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Lajhar> lajharok = new List<Lajhar>();
+
+            string[] lines = File.ReadAllLines("lajharok.txt");
+
+            for (int i = 0; i < lines.Length; i += 3)
+            {
+                var nameAndAge = lines[i].Split(';');
+                var name = nameAndAge[0];
+                var age = int.Parse(nameAndAge[1]);
+
+                var weight = double.Parse(lines[i + 1]);
+
+                var genderAndOffspring = lines[i + 2].Split(';');
+                var gender = genderAndOffspring[0];
+                var offspring = int.Parse(genderAndOffspring[1]);
+
+                lajharok.Add(new Lajhar
+                {
+                    Nev = name,
+                    Eletkor = age,
+                    Testtomeg = weight,
+                    Nem = gender,
+                    Utodok = offspring
+                });
+            }
+
+            var noStenyekSzama = lajharok.Count(l => l.Nem == "nosteny");
+            Console.WriteLine($"Nőstény lajhárok száma: {noStenyekSzama}");
+
+            var himekKolykei = lajharok.Where(l => l.Nem == "him")
+                                        .Sum(l => l.Utodok);
+            Console.WriteLine($"Hímek kölykei összesen: {himekKolykei}");
+
+            var rendezes = lajharok.OrderBy(l => l.Eletkor).ToList();
+            Console.WriteLine("Lajhárok életkor szerint rendezetten:");
+            foreach (var lajh in rendezes)
+            {
+                Console.WriteLine($"{lajh.Nev}, {lajh.Eletkor} éves");
+            }
+            var sikeresParok = lajharok.Where(l => l.Utodok > 0)
+                                       .GroupBy(l => l.Utodok)
+                                       .Select(g => new { UtodokSzama = g.Key, Parok = g.ToList() })
+                                       .ToList();
+            Console.WriteLine("Sikeres párok:");
+            foreach (var par in sikeresParok)
+            {
+                Console.WriteLine($"Pár {par.UtodokSzama} kölyökkel: ");
+                foreach (var parLajh in par.Parok)
+                {
+                    Console.WriteLine($"- {parLajh.Nev}, {parLajh.Nem}");
+                }
+            }
+        }
+    }
+}
---
+++ C# console/0217/0217/auto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0217
+{
+    class Auto
+    {
+        public required string Tipus { get; set; }
+        public int FutottKm { get; set; }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            List<Auto> autok = new List<Auto>();
+
+            string[] lines = File.ReadAllLines("autok2sorban.txt");
+
+
+            for (int i = 0; i < lines.Length; i += 2)
+            {
+                var tipus = lines[i];
+                var km = int.Parse(lines[i + 1]);
+
+                autok.Add(new Auto
+                {
+                    Tipus = tipus,
+                    FutottKm = km
+                });
+            }
+
+            var kevesebbMint20000 = autok.Count(a => a.FutottKm < 20000);
+            Console.WriteLine($"Autók, melyek 20000 km-nél kevesebbet futottak: {kevesebbMint20000}");
+
+            var toyotaAutok = autok.Count(a => a.Tipus.Contains("Toyota"));
+            Console.WriteLine($"Toyota autók száma: {toyotaAutok}");
+
+            var legtobbKm = autok.OrderByDescending(a => a.FutottKm).First();
+            Console.WriteLine($"A legtöbbet futott autó: {legtobbKm.Tipus}, {legtobbKm.FutottKm} km");
+
+            var atlagKm = autok.Average(a => a.FutottKm);
+            Console.WriteLine($"Autók átlagos futott km: {atlagKm}");
+        }
+    }
+}

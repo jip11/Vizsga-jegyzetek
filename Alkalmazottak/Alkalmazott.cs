5138b6411352990d0d45f61cf6ab00288130b722
Gregesz <58308465+jip11@users.noreply.github.com>
2025-03-19 07:03:11 +0100

    C# konzol

---
+++ Alkalmazottak/Alkalmazott.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alkalmazottak
+{
+    internal class Alkalmazott
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public int salary { get; set; }
+        public string department { get; set; }
+
+        // Konstruktor duh
+        public Alkalmazott(int Id, string Name, int Age, int Salary, string Department)
+        {
+            id = Id;
+            name = Name; 
+            age = Age; 
+            salary = Salary; 
+            department = Department;
+        }
+
+        public static List<Alkalmazott> initializeAlkalmazottListFromFile(string fileName = "")
+        {
+            //Szokásos fájlbeolvasós cuccok
+            FileStream fileStream = new FileStream(fileName, FileMode.Open);
+            StreamReader streamReader = new StreamReader(fileStream);
+
+            // Ebben tároljuk az alkalmazottakat
+            List<Alkalmazott> resultList = new List<Alkalmazott>();
+
+            // Első sor átugrása, mert az fejléc
+            string line = streamReader.ReadLine();
+
+            // Második sor beolvasása
+            line = streamReader.ReadLine();
+
+            while (line != null)
+            {
+                string[] splittedLine = line.Split('\t'); // Tabulátor mentén darabolni
+
+                resultList.Add  // Hozzáadjuk a listához a lent példányosított Alkalmazott objektumot
+                (
+                    new Alkalmazott
+                    (
+                        int.Parse(splittedLine[0]), // id
+                        splittedLine[1],            // name
+                        int.Parse(splittedLine[2]), // age
+                        int.Parse(splittedLine[3]), // salary
+                        splittedLine[4]             // department
+                    )
+                );
+
+                line = streamReader.ReadLine(); // Új sor beolvasása
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ Alkalmazottak/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Alkalmazottak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. feladat
+            List<Alkalmazott> alkalmazottList = new List<Alkalmazott>();
+            alkalmazottList = Alkalmazott.initializeAlkalmazottListFromFile("alkalmazottak.csv");
+            Console.WriteLine($"1. feladat: Beolvasott alkalmazottak száma: {alkalmazottList.Count}");
+
+            // 2. feladat
+            Dictionary<string, int> employeePerDepartment = new Dictionary<string, int>();
+            alkalmazottList.ForEach(alkalmazott =>  // Tipikus lista.ForEach()
+            {
+                if (employeePerDepartment.ContainsKey(alkalmazott.department))  // Megnézzük, hogy a dictionary-be felvettük-e már azt a department-et
+                {
+                    employeePerDepartment[alkalmazott.department]++;    // Ha igen, akkor csak növeljük meg az értéket
+                } else
+                {
+                    employeePerDepartment.Add(alkalmazott.department, 1);   // Ha nem, akkor adjuk hozzá, és társítunk mellé 1-es értéket (mert ez az első, amit találtunk)
+                }
+            });
+
+            Console.WriteLine("2. feladat: Osztályonkénti alkalmazottak száma:");
+            foreach (var pair in employeePerDepartment) // Dictionary-ban nincs külön ForEach metódus; itt a pair egy objektum, aminek van Key és Value adattagja
+            {
+                Console.WriteLine($"    {pair.Key}: {pair.Value}");
+            }
+
+            // 3. feladat
+            Console.WriteLine("3. feladat: 500000-nél magasabb fizetésű alkalmazottak:");
+            alkalmazottList.ForEach((alkalmazott) =>
+            {
+                if (alkalmazott.salary > 500000)
+                {
+                    Console.WriteLine($"    {alkalmazott.name}, {alkalmazott.salary}");
+                }
+            });
+
+            // 4. feladat
+            double averageSalary = 0.0;
+            int itCounter = 0;  // Az átlagszámításhoz kell majd
+
+            alkalmazottList.ForEach((alkalmazott) =>
+            {
+                if (alkalmazott.department == "IT")
+                {
+                    itCounter++;
+                    averageSalary += Convert.ToDouble(alkalmazott.salary);
+                }
+            });
+
+            Console.WriteLine($"4. feladat: IT osztály átlagfizetése: {averageSalary / itCounter}");
+
+            // 5. feladat
+            Alkalmazott highestPayed = null;    // Ebben tartjuk nyilván a legmagasabb fizetéssel rendelkező alkalmazottat, egyelőre null mert nem vizsgáltunk semmit.
+
+            alkalmazottList.ForEach((alkalmazott) =>
+            {
+                if (highestPayed == null || alkalmazott.salary > highestPayed.salary) // Ha a highestPayed null, vagy a vizsgált alkalmazottnak magasabb a fizetése, mint a jelenleg nyilvántartottnak
+                {
+                    highestPayed = alkalmazott;
+                }
+            });
+
+            Console.WriteLine($"5. feladat: Legmagasabb fizetésű alkalmazott: {highestPayed.name}, {highestPayed.salary}");
+
+            // 6. feladat
+            Alkalmazott youngestEmployee = null;    // Ebbenm tartjuk nyilván a legfiatalabb alkalmazottat, egyelőre null mert nem vizsgáltunk semmit.
+
+            alkalmazottList.ForEach((alkalmazott) =>
+            {
+                if (youngestEmployee == null || alkalmazott.age < youngestEmployee.age) // Ha a youngestEmployee null, vagy a vizsgált alkalmazott fiatalabb, mint a jelenleg nyilvántartott
+                {
+                    youngestEmployee = alkalmazott;
+                }
+            });
+
+            Console.WriteLine($"6. feladat: Legfiatalabb alkalmazott: {youngestEmployee.name}, {youngestEmployee.age} éves");
+        }
+    }
+}
